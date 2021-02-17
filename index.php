ff4b05378ed342fafb4e04ead1f9b6a4157c26d3
Edson Famanda <edson@equipmoz.org>
2021-02-17 11:34:08 +0200

    Working just fine by inserting Url on a Form

--- index.php
+++ index.php
@@ -8,29 +8,39 @@
 
 <h1>CORS CHECKER </h1>
 
-<p style="color:red"> Also check out your console</p>
+<h2>Insert the URL and Check all the data below!!!</h2>
+<p></p>
+<form id="idForm" method="post">
+    <input type="text" id="url" placeholder="URL">
+    <button type="submit">Submit</button>
+</form>
 
-<!--<button type="button" onclick="click()"> Clica aqui</button>-->
-
+<hr>
+<h3 id="data"></h3>
 <p id="list"></p>
+
+
 <script type="text/javascript">
-    let url = 'http://127.0.0.1:8100/api/respostas'
+    $('#idForm').submit(function(e) {
+        e.preventDefault(); // avoid to execute the actual submit of the form.
+        var url = $("#url").val();
+        console.log("LINK: " + url)
 
-    console.log("LINK: " + url)
-
-    $.ajax({
-        type: "GET",
-        dataType: "json",
-        url: url,
-        success: function(data){
-            console.log(data);
-
-            let array = JSON.stringify(data)
-            document.getElementById("list").innerHTML = array;
-
-        }
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: url,
+            success: function (data) {
+                console.log(data);
+                    $('#data').text('Response Success => ' + url);
+                    $('#list').text(JSON.stringify(data));
+            },
+            error: function(error) {
+                $('#data').text('Response Error => ' + url);
+                $('#list').text(error.statusText != 'error' ? error.statusText : "Blocked by CORS policy");
+            },
+        });
     });
-
 </script>
 
 </body>
