ea1625755e7eac6307e0860655114e0a1730745e
Edson Famanda <edson@equipmoz.org>
2021-02-17 10:11:05 +0200

    Converting JSON(Object) to String

--- index.php
+++ index.php
@@ -8,9 +8,13 @@
 
 <h1>CORS CHECKER </h1>
 
-<p style="color:red"> Check out your console</p>
+<p style="color:red"> Also check out your console</p>
+
+<!--<button type="button" onclick="click()"> Clica aqui</button>-->
+
+<p id="list"></p>
 <script type="text/javascript">
-    let url = 'http://127.0.0.1:8000/api/teste'
+    let url = 'http://127.0.0.1:8100/api/respostas'
 
     console.log("LINK: " + url)
 
@@ -20,6 +24,10 @@
         url: url,
         success: function(data){
             console.log(data);
+
+            let array = JSON.stringify(data)
+            document.getElementById("list").innerHTML = array;
+
         }
     });
 
